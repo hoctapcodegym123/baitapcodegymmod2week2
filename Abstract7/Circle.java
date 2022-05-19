a43ac4f474709b98f563fcdc553c43d67eef9be4
hoanghai1310 <hoanghai.131096@gmail.com>
2022-05-19 10:18:04 +0700

    comparable

---
+++ Abstract7/Circle.java
@@ -0,0 +1,46 @@
+package Abstract7;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "Green";
+    private boolean filled = true;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+}
+
