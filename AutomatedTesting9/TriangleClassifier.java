ca252e3fc5ef19d29d5acb910e5654ce3f8636d3
hoanghai1310 <hoanghai.131096@gmail.com>
2022-05-20 14:04:53 +0700

    triangleclass

--- AutomatedTesting9/TriangleClassifier.java
+++ AutomatedTesting9/TriangleClassifier.java
@@ -1,2 +1,19 @@
-package AutomatedTesting9;public class TriangleClassifier {
+package AutomatedTesting9;
+
+public class TriangleClassifier {
+    public static int Triangle(int a, int b, int c) {
+        if (a == b && b == c) {
+        }
+        return 0;
+    }
+    public static int Trianglecan(int a, int b, int c) {
+        if (a == b && b == c) {
+        }
+        return 0;
+    }
+    public static int Trianglethuong(int a, int b, int c) {
+        if (a != b && b != c && a!=c) {
+        }
+        return 0;
+    }
 }
--- AutomatedTesting9/TriangleClassifierTest.java
+++ AutomatedTesting9/TriangleClassifierTest.java
@@ -1,2 +1,42 @@
-package AutomatedTesting9;public class TriangleClassifierTest {
+package AutomatedTesting9;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TriangleClassifierTest {
+    @Test
+    @DisplayName("Testing tam giac deu")
+    public void testTriangledeu() {
+        int a = 2;
+        int b = 2;
+        int c = 2;
+        int expected = 0;
+
+        int result = TriangleClassifier.Triangle(a,b,c);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("Testing tam giac can")
+    public void testTrianglecan() {
+        int a = 2;
+        int b = 2;
+        int c = 3;
+        int expected = 0;
+
+        int result = TriangleClassifier.Trianglecan(a,b,c);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("Testing tam giac thuong")
+    public void testTrianglethuong() {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int expected = 0;
+
+        int result = TriangleClassifier.Trianglethuong(a,b,c);
+        assertEquals(expected,result);
+    }
 }
